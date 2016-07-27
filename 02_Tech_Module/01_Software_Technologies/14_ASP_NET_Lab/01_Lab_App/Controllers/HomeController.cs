1245982848456c07de59f402a0dc15b031a93868
Sideroff <ivan.sideroff@gmail.com>
2016-07-27 23:54:19 +0300

    added ASP .NET MVC Lab to Software Technologies

---
+++ 02_Tech_Module/01_Software_Technologies/14_ASP_NET_Lab/01_Lab_App/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _01_Lab_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Numbers()
+        {
+            return View();
+        }
+        public ActionResult Files(string path = @"C:\")
+        {
+            var files = Directory.GetDirectories(path).ToList();
+            files.AddRange(Directory.GetFiles(path).ToList());
+            return View(files);
+        }
+    }
+}
